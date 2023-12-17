c4035932eafcf3c443e7515bb6375080af01de30
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2023-12-17 23:56:34 +0530

    Create audi_turtle_basic.py

---
+++ audi_turtle_basic.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+r = 50
+pensize(10)
+penup()
+setpos(r*-2,0)
+pendown()
+circle(r)
+penup()
+setpos(r*-2+75*1,0)
+pendown()
+circle(r)
+penup()
+setpos(r*-2+75*2,0)
+pendown()
+circle(r)
+penup()
+setpos(r*-2+75*3,0)
+pendown()
+circle(r)
